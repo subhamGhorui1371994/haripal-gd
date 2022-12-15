a82584de60ffc6f9d47dd911531be875e59508e8
subham-codesigma <subham.ghorui@codesigma.tech>
2022-12-15 23:40:48 +0530

    change

--- resources/views/layouts/template-header.blade.php
+++ resources/views/layouts/template-header.blade.php
@@ -7,7 +7,7 @@
                     <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
                         <div class="header-top-left">
                             <ul>
-                                <li><i class="fa fa-phone" aria-hidden="true"></i><a href="Tel:{{$siteSettings->phone ?? '+ 123 456 78910'}}"> {{$siteSettings->phone ?? '+ 123 456 78910'}} </a></li>
+{{--                                <li><i class="fa fa-phone" aria-hidden="true"></i><a href="Tel:{{$siteSettings->phone ?? '+ 123 456 78910'}}"> {{$siteSettings->phone ?? '+ 123 456 78910'}} </a></li>--}}
                                 <li><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:{{$siteSettings->email ?? 'bagnanheritage@gmail.com'}}">{{$siteSettings->email ?? 'bagnanheritage@gmail.com'}}</a></li>
                             </ul>
                         </div>
@@ -40,16 +40,18 @@
                     </div>
                     <div class="col-xl-9 col-lg-9 col-md-9 col-sm-12">
                         <div class="header-top-right text-uppercase">
-                            <h1>Heritage College of Education</h1>
+                            <h1>HARIPAL GURUDAYAL INSTITUTION</h1>
                         </div>
                         <div class="header-top-right text-uppercase mb-2">
                             <ul>
-                                <li>(B.ED) And (D.EL.ED)</li>
-                                <li>RABIBHAG * BAGNAN * HOWRAH * 711312</li>
+                                <li>(Secondary ) And (Higher secondary)</li>
+                                <li>KHAMARCHANDI * CHANDANNAGORE * HARIPAL * HOOGHLY * 712405</li>
                             </ul>
                             <ul>
-                                <li>(APPROVED BY N.C.T.E. & AFFILIATED TO - W.B.U.T.T.E.P.A & W.B.B.P.E)</li>
-                                <li>ESTD : 2014</li>
+                                <li>DIST CODE: 19120913003</li>
+                                <li>INDEX NO: G4-034</li>
+                                <li>CODE NO: 117071</li>
+                                <li>ESTD : 1899</li>
                             </ul>
                         </div>
                     </div>
@@ -62,14 +64,33 @@
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
                         <nav id="desktop-nav">
                             <ul>
+{{--                                <li class="{{Request::segment(1) === null ? 'active' : ''}}"><a href="{!! url('/') !!}">Home</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'courses' ? 'active' : ''}}"><a href="{!! url('/courses') !!}">Courses</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'gallery' ? 'active' : ''}}"><a href="{!! url('/gallery') !!}">Gallery</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'resource-person' ? 'active' : ''}}"><a href="{!! url('resource-person') !!}">Resource Person</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'student-portal' ? 'active' : ''}}"><a href="{!! url('student-portal') !!}">Student Portal</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'infrastructure' ? 'active' : ''}}"><a href="{!! url('infrastructure') !!}">Infrastructure</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'mandatory-disclosure' ? 'active' : ''}}"><a href="{!! url('mandatory-disclosure') !!}">Mandatory Disclosure</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'biometric' ? 'active' : ''}}"><a href="{!! url('biometric') !!}">Biometric Attendance</a></li>--}}
+{{--                                <li class="{{Request::segment(1) ==='notice' ? 'active' : ''}}"><a href="{!! url('notice') !!}">Notice</a>--}}
+{{--                                    <ul><li><a href="{!! url('events') !!}">Event</a></li></ul>--}}
+{{--                                </li>--}}
+{{--                                <li class="{{Request::segment(1) === 'contact' ? 'active' : ''}}"><a href="{!! url('contact') !!}">Contact</a></li>--}}
                                 <li class="{{Request::segment(1) === null ? 'active' : ''}}"><a href="{!! url('/') !!}">Home</a></li>
+                                <li class="{{Request::segment(1) === 'about-us' ? 'active' : ''}}"><a href="{!! url('about-us') !!}">About Us</a></li>
+                                <li class="{{Request::segment(1) === 'resource-person' ? 'active' : ''}}"><a href="{!! url('resource-person') !!}">Staff</a></li>
+
+                                <li class="{{Request::segment(1) ==='achievement' ? 'active' : ''}}"><a href="{!! url('co-curricular') !!}">Achievement</a>
+                                    <ul>
+                                        <li><a href="{!! url('co-curricular') !!}">Co-curricular</a></li>
+                                        <li><a href="{!! url('academic') !!}">Academic</a></li>
+                                    </ul>
+                                </li>
+
+                                <li class="{{Request::segment(1) === 'activities' ? 'active' : ''}}"><a href="{!! url('activities') !!}">Activities</a></li>
                                 <li class="{{Request::segment(1) === 'courses' ? 'active' : ''}}"><a href="{!! url('/courses') !!}">Courses</a></li>
-                                <li class="{{Request::segment(1) === 'gallery' ? 'active' : ''}}"><a href="{!! url('/gallery') !!}">Gallery</a></li>
-                                <li class="{{Request::segment(1) === 'resource-person' ? 'active' : ''}}"><a href="{!! url('resource-person') !!}">Resource Person</a></li>
-                                <li class="{{Request::segment(1) === 'student-portal' ? 'active' : ''}}"><a href="{!! url('student-portal') !!}">Student Portal</a></li>
-                                <li class="{{Request::segment(1) === 'infrastructure' ? 'active' : ''}}"><a href="{!! url('infrastructure') !!}">Infrastructure</a></li>
-                                <li class="{{Request::segment(1) === 'mandatory-disclosure' ? 'active' : ''}}"><a href="{!! url('mandatory-disclosure') !!}">Mandatory Disclosure</a></li>
-                                <li class="{{Request::segment(1) === 'biometric' ? 'active' : ''}}"><a href="{!! url('biometric') !!}">Biometric Attendance</a></li>
+                                <li class="{{Request::segment(1) === 'biometric' ? 'active' : ''}}"><a href="{!! url('biometric') !!}">Admission</a></li>
+
                                 <li class="{{Request::segment(1) ==='notice' ? 'active' : ''}}"><a href="{!! url('notice') !!}">Notice</a>
                                     <ul><li><a href="{!! url('events') !!}">Event</a></li></ul>
                                 </li>
@@ -89,17 +110,26 @@
                     <div class="mobile-menu">
                         <nav id="dropdown">
                             <ul>
+{{--                                <li class="{{Request::segment(1) === null ? 'active' : ''}}"><a href="{!! url('/') !!}">Home</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'courses' ? 'active' : ''}}"><a href="{!! url('/courses') !!}">Courses</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'gallery' ? 'active' : ''}}"><a href="{!! url('/gallery') !!}">Gallery</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'resource-person' ? 'active' : ''}}"><a href="{!! url('resource-person') !!}">Resource Person</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'student-portal' ? 'active' : ''}}"><a href="{!! url('student-portal') !!}">Student Portal</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'infrastructure' ? 'active' : ''}}"><a href="{!! url('infrastructure') !!}">Infrastructure</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'mandatory-disclosure' ? 'active' : ''}}"><a href="{!! url('mandatory-disclosure') !!}">Mandatory Disclosure</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'biometric' ? 'active' : ''}}"><a href="{!! url('biometric') !!}">Biometric Attendance</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'notice' ? 'active' : ''}}"><a href="{!! url('notice') !!}#">Notice</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'events' ? 'active' : ''}}"><a href="{!! url('events') !!}#">Event</a></li>--}}
+{{--                                <li class="{{Request::segment(1) === 'contact' ? 'active' : ''}}"><a href="{!! url('contact') !!}">Contact</a></li>--}}
+
                                 <li class="{{Request::segment(1) === null ? 'active' : ''}}"><a href="{!! url('/') !!}">Home</a></li>
-                                <li class="{{Request::segment(1) === 'courses' ? 'active' : ''}}"><a href="{!! url('/courses') !!}">Courses</a></li>
-                                <li class="{{Request::segment(1) === 'gallery' ? 'active' : ''}}"><a href="{!! url('/gallery') !!}">Gallery</a></li>
-                                <li class="{{Request::segment(1) === 'resource-person' ? 'active' : ''}}"><a href="{!! url('resource-person') !!}">Resource Person</a></li>
-                                <li class="{{Request::segment(1) === 'student-portal' ? 'active' : ''}}"><a href="{!! url('student-portal') !!}">Student Portal</a></li>
-                                <li class="{{Request::segment(1) === 'infrastructure' ? 'active' : ''}}"><a href="{!! url('infrastructure') !!}">Infrastructure</a></li>
-                                <li class="{{Request::segment(1) === 'mandatory-disclosure' ? 'active' : ''}}"><a href="{!! url('mandatory-disclosure') !!}">Mandatory Disclosure</a></li>
-                                <li class="{{Request::segment(1) === 'biometric' ? 'active' : ''}}"><a href="{!! url('biometric') !!}">Biometric Attendance</a></li>
-                                <li class="{{Request::segment(1) === 'notice' ? 'active' : ''}}"><a href="{!! url('notice') !!}#">Notice</a></li>
-                                <li class="{{Request::segment(1) === 'events' ? 'active' : ''}}"><a href="{!! url('events') !!}#">Event</a></li>
-                                <li class="{{Request::segment(1) === 'contact' ? 'active' : ''}}"><a href="{!! url('contact') !!}">Contact</a></li>
+                                <li class="{{Request::segment(1) === 'history' ? 'active' : ''}}"><a href="{!! url('/history') !!}">History</a></li>
+                                <li class="{{Request::segment(1) === 'administration' ? 'active' : ''}}"><a href="{!! url('/administration') !!}">Administration</a></li>
+                                <li class="{{Request::segment(1) === 'resource-person' ? 'active' : ''}}"><a href="{!! url('resource-person') !!}">Staff</a></li>
+                                <li class="{{Request::segment(2) === 'co-curricular' ? 'active' : ''}}"><a href="{!! url('/achievement/co-curricular') !!}"> Co-curricular Achievement </a></li>
+                                <li class="{{Request::segment(2) === 'academic ' ? 'active' : ''}}"><a href="{!! url('/achievement/academic') !!}"> Academic  Achievement </a></li>
+                                <li class="{{Request::segment(1) === 'activities' ? 'active' : ''}}"><a href="{!! url('activities') !!}">Activities</a></li>
+
                             </ul>
                         </nav>
                     </div>
